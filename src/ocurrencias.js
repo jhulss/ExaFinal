ca316bc67fb320f9f6061c8b3ac1cc71a0e4b308
Jhulians Garcia Hinojosa <jhulians.garcia@ucb.edu.bo>
2023-06-13 10:15:22 -0400

    Devolver una sola palabra

---
+++ src/ocurrencias.js
@@ -0,0 +1,21 @@
+class Ocurrencia{
+    contarOcurrencia(frase)
+    {
+        let resp = "";
+        if (!frase)
+        {
+            resp = "Introduce una frase para continuar";
+        }
+        for (let i = i; frase.lenth(); i++)
+        {
+            
+        }
+
+
+        return frase;
+    }
+
+
+}
+
+export default Ocurrencia;
